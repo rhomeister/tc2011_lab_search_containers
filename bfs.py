69d9215415723377cb54f641e6ed445c78db6cb3
Yael Arturo Villanueva <LordOfVultures@users.noreply.github.com>
2016-09-20 18:10:55 -0500

    Update bfs.py
    added stdin suport :D

--- bfs.py
+++ bfs.py
@@ -1,1 +1,15 @@
+import fileinput
 
+queue = []
+
+input = fileinput.input()
+i = 0
+for line in input:
+  if i = 0:
+    max_stack = int(line.strip())
+  if i = 1:
+    intial_state = line.split(";")
+  if i = 2:
+    end_state = line.split(";")
+  i += 1
+  
