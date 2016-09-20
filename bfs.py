03d019af4eb719cd18914445836cbba542ba031c
Yael Arturo Villanueva <LordOfVultures@users.noreply.github.com>
2016-09-20 18:54:29 -0500

    Update bfs.py
    seting up the input format required to solve the problem (not complete)

--- bfs.py
+++ bfs.py
@@ -7,9 +7,23 @@ i = 0
 for line in input:
   if i == 0:
     max_stack = int(line.strip())
+    print (max_stack)
   if i == 1:
-    intial_state = line.split(";")
+    j = 0
+    initial_state = line.strip()
+    initial_state = initial_state.replace(" ", "")
+    initial_state = initial_state.replace("(", "")
+    initial_state = initial_state.replace(")", "")
+    initial_state = initial_state.split(';')
+    try:
+      while initial_state[j] is not None:
+        list(initial_state[j])
+        j += 1
+    except IndexError:
+    	pass
+    print(initial_state)
   if i == 2:
-    end_state = line.split(";")
+    end_state = line.strip()
+    
+    print(end_state)
   i += 1
-  
