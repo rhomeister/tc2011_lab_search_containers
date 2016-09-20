bede5631b373d18bcfe4ff2942f80e5faa1e4eae
Yael Arturo Villanueva <LordOfVultures@users.noreply.github.com>
2016-09-20 18:12:30 -0500

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -5,11 +5,11 @@ queue = []
 input = fileinput.input()
 i = 0
 for line in input:
-  if i = 0:
+  if i == 0:
     max_stack = int(line.strip())
-  if i = 1:
+  if i == 1:
     intial_state = line.split(";")
-  if i = 2:
+  if i == 2:
     end_state = line.split(";")
   i += 1
   
